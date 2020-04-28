c0c1ecdc67fb1f84b369c2f79be62b56d264f08d
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-28 23:58:22 +0530

    REFACTORED: Code to validate password

--- src/main/java/com/demo/LoginServlet.java
+++ src/main/java/com/demo/LoginServlet.java
@@ -17,13 +17,14 @@ import java.util.regex.Pattern;
 )
 public class LoginServlet extends HttpServlet {
     private final String userNamePattern = "^[A-Z][a-zA-z]{2,}$";
+    private final String passwordPattern="^(?=.{8,})(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]*[^A-Za-z0-9][A-Za-z0-9]*$";
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String inputUserName = req.getParameter("user");
-        String inputPassword = req.getParameter("pwd");
+        String inputUserName = req.getParameter("userName");
+        String inputPassword = req.getParameter("password");
         String password = "pass";
-        if (Pattern.matches(userNamePattern, inputUserName) && password.equals(inputPassword)) {
+        if (Pattern.matches(userNamePattern, inputUserName) && Pattern.matches(passwordPattern,inputPassword)) {
             req.setAttribute("user", inputUserName);
             req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
         } else {
